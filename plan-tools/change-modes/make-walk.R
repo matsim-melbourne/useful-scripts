c7320ea1831f5a0775589a6d4ce8574483bcac69
jafshin <jxafshin@gmail.com>
2022-08-24 17:16:27 +1000

    finding walkable tours

--- plan-tools/change-modes/make-walk.R
+++ plan-tools/change-modes/make-walk.R
@@ -8,12 +8,40 @@ library(tidyverse)
 trips <- read_delim("output_trips.csv.gz",delim = ";")
 
 # Identifying home-tours so that we change the mode for the whole tour
-trips_sampled <- trips %>% slice_head(n=1000) %>% 
+trips_sampled <- trips %>% slice_head(n=1000) %>%
   dplyr::select(person, trip_number, modes,start_activity_type,end_activity_type)
+
 tour_id = 0
+old_person = 0
+
 for(i in 1:nrow(trips_sampled) ){
-  
+
+  this_person <- trips_sampled[i, "person"]
+
+  if(old_tour_id == tour_id){
+  # still in the same tour
+    if(this_person != old_person){
+      # Person did not return home, moved to the next person
+      tour_id <- tour_id + 1
+    }
+  }
+
   trips_sampled[i, "tour_id"] <- tour_id
-  if(trips_sampled$end_activity_type[i] == "Home") tour_id = tour_id + 1 
-  # TODO if not returned home, change the tour_id regardless
+
+  old_tour_id <- tour_id
+
+  if(trips_sampled$end_activity_type[i] == "Home"){
+    # Person returned home, tour finished
+    tour_id <- tour_id + 1
+  }
 }
+
+# getting tour distances
+tours_distance <- trips_sampled %>%
+  group_by(tour_id) %>%
+  summarise(total_distance=sum(distance)/1000)
+
+# Finding tours within walking distance of 1.6 km
+tours_walkable_id <- tours_distance %>%
+  filter(total_distance < 1.6 ) %>%
+  distinct(tour_id)
